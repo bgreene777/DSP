21ac7825d730cd17e554ceca21be26f636654957
Brian Greene <bgreene777@gmail.com>
2018-08-23 17:24:49 -0500

    modified for Q1.1-1.5

--- HW1/P1_1.m
+++ HW1/P1_1.m
@@ -1,12 +1,12 @@
-% Program P1_1
-% Generation of a Unit Sample Sequence 
+% Program P1_1 - modified
+% Generation of an advanced unit step sequence
 clf;
 % Generate a vector from -10 to 20
 n = -10:20;
 % Generate the unit sample sequence
-u = [zeros(1,10) 1 zeros(1,20)];
+sd = [zeros(1,3) ones(1,28)];
 % Plot the unit sample sequence
-stem(n,u);
+stem(n,sd);
 xlabel('Time index n');ylabel('Amplitude');
-title('Unit Sample Sequence');
+title('Unit Step Sequence Advanced 7');
 axis([-10 20 0 1.2]);
