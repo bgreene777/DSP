58da34242a10c7ae41ea819c9838a84902d2b122
bgreene777 <bgreene777@gmail.com>
2018-08-25 18:39:06 -0500

    1_6 project 1.6

--- HW1/P1_6.m
+++ HW1/P1_6.m
@@ -2,9 +2,10 @@
 % Generation of amplitude modulated sequence
 clf;
 n = 0:100;	
-m = 0.4;fH = 0.1; fL = 0.01;				
+m = 0.4;fH = 0.1; fL = 0.05;				
 xH = sin(2*pi*fH*n);
 xL = sin(2*pi*fL*n);
 y = (1+m*xL).*xH;
 stem(n,y);grid;
 xlabel('Time index n');ylabel('Amplitude');
+title('m=0.4, fH=0.1, fL=0.05')
