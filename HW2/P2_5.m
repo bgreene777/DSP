84d5ff201230d4d09e6b64765b06f786211adf57
bgreene777 <bgreene777@gmail.com>
2018-09-06 00:19:05 -0500

    finished project 2.5

--- HW2/P2_5.m
+++ HW2/P2_5.m
@@ -1,11 +1,13 @@
-% Program P2_5
+% Program P2_5 - modified for Q2.22
 % Compute the impulse response y
 clf;
 N = 40;
+% y[n]?0.4y[n?1]+0.75y[n?2]=2.2403x[n]+2.4908x[n?1]+2.2403x[n?2]
 num = [2.2403 2.4908 2.2403];
-den = [1 -0.4 0.75];
-y = impz(num,den,N);
+den = [1.0 -0.4 0.75];
+step = ones(1, N);
+y = filter(num,den,step);
 % Plot the impulse response
 stem(y);
 xlabel('Time index n'); ylabel('Amplitude');
-title('Impulse Response'); grid;
+title('Step Response'); grid;
