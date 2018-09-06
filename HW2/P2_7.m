d27ff424d3e9bcc47ed2a8cae957ba2de8c7662b
bgreene777 <bgreene777@gmail.com>
2018-09-06 00:50:12 -0500

    finished project 2.7

--- HW2/P2_7.m
+++ HW2/P2_7.m
@@ -1,14 +1,14 @@
-% Program P2_7
+% Program P2_7 - modified for Q2.29
 clf;
-h = [3 2 1 -2 1 0 -4 0 3];	% impulse response
-x = [1 -2 3 -4 3 2 1];		% input sequence
+h = [1 -2 3 4 -3 2 -1 1 3 6 -3 4 5 2 -1];	% impulse response
+x = [2 4 5 3 -2 -1 3 -2 9 7];		% input sequence
 y = conv(h,x);
-n = 0:14;
+n = 0:23;
 subplot(2,1,1);
 stem(n,y);
 xlabel('Time index n'); ylabel('Amplitude');
 title('Output Obtained by Convolution'); grid;
-x1 = [x zeros(1,8)];
+x1 = [x zeros(1,14)];
 y1 = filter(h,1,x1);
 subplot(2,1,2);
 stem(n,y1);
