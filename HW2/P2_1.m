2ad9e28e2dfa807a5ef65645f98fe435b60af368
bgreene777 <bgreene777@gmail.com>
2018-09-05 21:57:28 -0500

    finished Project 2.1

--- HW2/P2_1.m
+++ HW2/P2_1.m
@@ -1,34 +1,27 @@
-% Program P2_1
+% Program P2_1 - swept frequency Q2.4
 % Simulation of an M-point Moving Average Filter
 % Generate the input signal
 n = 0:100;
-s1 = cos(2*pi*0.05*n); % A low-frequency sinusoid
-s2 = cos(2*pi*0.47*n); % A high frequency sinusoid
-x = s1+s2;
+a = pi/200;
+b = 0;
+arg = a*n.*n + b*n;
+x = cos(arg);
 % Implementation of the moving average filter
 M = input('Desired length of the filter = ');
 num = ones(1,M);
+%num = (-1) .^ (0:M-1);
 y = filter(num,1,x)/M;
 % Display the input and output signals
 clf;
-subplot(2,2,1);
-plot(n, s1);
-axis([0, 100, -2, 2]);
-xlabel('Time index n'); ylabel('Amplitude');
-title('Signal #1');
-subplot(2,2,2);
-plot(n, s2);
-axis([0, 100, -2, 2]);
-xlabel('Time index n'); ylabel('Amplitude');
-title('Signal #2');
-subplot(2,2,3);
+subplot(2,1,1);
 plot(n, x);
 axis([0, 100, -2, 2]);
 xlabel('Time index n'); ylabel('Amplitude');
-title('Input Signal');
-subplot(2,2,4);
+title('Signal Input');
+subplot(2,1,2);
 plot(n, y);
 axis([0, 100, -2, 2]);
 xlabel('Time index n'); ylabel('Amplitude');
-title('Output Signal');	
+title('Signal Output');
+suptitle('Swept Frequency Q2.4');
 axis;
