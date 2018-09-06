8bcab72acd54b87d19513958ee09ada8d33fa5d7
bgreene777 <bgreene777@gmail.com>
2018-09-05 23:41:32 -0500

    done with project 2.4

--- HW2/P2_4.m
+++ HW2/P2_4.m
@@ -1,16 +1,19 @@
-% Program P2_4
+% Program P2_4 - modified for Q2.17
 % Generate the input sequences
 clf;
 n = 0:40; D = 10;a = 3.0;b = -2;
 x = a*cos(2*pi*0.1*n) + b*cos(2*pi*0.4*n);
 xd = [zeros(1,D) x];
-num = [2.2403 2.4908 2.2403];
-den = [1 -0.4 0.75];
-ic = [0 0]; % Set initial conditions
+y = zeros(1, 41);
+yd = zeros(1, length(xd));
 % Compute the output y[n]
-y = filter(num,den,x,ic);
+for i = 2:41
+    y(i) = i * x(i) + x(i-1);
+end
 % Compute the output yd[n]
-yd = filter(num,den,xd,ic);
+for i = 2:length(xd)
+    yd(i) = i * xd(i) + xd(i-1);
+end
 % Compute the difference output d[n]
 d = y - yd(1+D:41+D);
 % Plot the outputs
@@ -21,8 +24,9 @@ title('Output y[n]'); grid;
 subplot(3,1,2)
 stem(n,yd(1:41));
 ylabel('Amplitude');
-title(['Output due to Delayed Input x[n Ð', num2str(D),']']); grid;
+title(['Output due to Delayed Input x[n-', num2str(D),']']); grid;
 subplot(3,1,3)
 stem(n,d);
 xlabel('Time index n'); ylabel('Amplitude');
 title('Difference Signal'); grid;
+suptitle('y[n] = nx[n] + x[n-1]');
