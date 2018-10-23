b0d7b30b3e0dc9385b98f77087e379fe09db856b
Brian Greene <bgreene777@gmail.com>
2018-10-22 20:12:04 -0500

    through Q 3.10

--- HW3/P3_1.m
+++ HW3/P3_1.m
@@ -1,9 +1,10 @@
-% Program P3_1
+% Program P3_1 modified for Q3.4
 % Evaluation of the DTFT 
 clf;
 % Compute the frequency samples of the DTFT
 w = -4*pi:8*pi/511:4*pi;
-num = [2 1];den = [1 -0.6];
+% N = 512;
+num = [1 3 5 7 9 11 13 15 17]; den = [1];
 h = freqz(num, den, w);
 % Plot the DTFT
 subplot(2,1,1)
@@ -23,7 +24,7 @@ title('Magnitude Spectrum |H(e^{j\omega})|')
 xlabel('\omega /\pi');
 ylabel('Amplitude');
 subplot(2,1,2)
-plot(w/pi,angle(h));grid
+plot(w/pi,180*angle(h)/pi);grid
 title('Phase Spectrum arg[H(e^{j\omega})]')
 xlabel('\omega /\pi');
-ylabel('Phase in radians');
+ylabel('Phase in degrees');
--- HW3/P3_2.m
+++ HW3/P3_2.m
@@ -1,19 +1,33 @@
-% Program P3_2
+% Program P3_2 modified for Q3.6
 % Time-Shifting Properties of DTFT
 clf;
-w = -pi:2*pi/255:pi; wo = 0.4*pi; D = 10;
-num = [1 2 3 4 5 6 7 8 9];
-h1 = freqz(num, 1, w);
-h2 = freqz([zeros(1,D) num], 1, w);
+w = -pi:2*pi/255:pi; % frequency vector
+wo = 0.4*pi; % fundamental frequency
+D = 15; % amount of time shift
+num = [1 2 3]; % input sequence
+h1 = freqz(num, 1, w); % DTFT of original sequence
+h2 = freqz([zeros(1,D) num], 1, w); % DTFT of time-shifted sequence
+% plot DTFT magnitude spectrum of original sequnce
 subplot(2,2,1)
 plot(w/pi,abs(h1));grid
 title('Magnitude Spectrum of Original Sequence')
+xlabel('\omega / \pi')
+ylabel('Amplitude')
+% plot DTFT magnitude spectrum of time-shifted sequence
 subplot(2,2,2)
 plot(w/pi,abs(h2));grid
 title('Magnitude Spectrum of Time-Shifted Sequence')
+xlabel('\omega / \pi')
+ylabel('Amplitude')
+% plot DTFT phase of original sequence
 subplot(2,2,3)
 plot(w/pi,angle(h1));grid
 title('Phase Spectrum of Original Sequence')
+xlabel('\omega / \pi')
+ylabel('Phase [radians]')
+% plot DTFT phase of time-shifted sequence
 subplot(2,2,4)
 plot(w/pi,angle(h2));grid
 title('Phase Spectrum of Time-Shifted Sequence')
+xlabel('\omega / \pi')
+ylabel('Phase [radians]')
